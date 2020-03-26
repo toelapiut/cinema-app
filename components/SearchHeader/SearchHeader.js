c534948168a5df455ad7f13601f07466a2c19ab6
toelapiut <toelapiut7@gmail.com>
2020-03-26 03:53:53 +0300

    feat: added search bar on the top next working on tags

--- components/SearchHeader/SearchHeader.js
+++ components/SearchHeader/SearchHeader.js
@@ -1,11 +1,39 @@
 import React from 'react';
-import {SearchHeaderWrapper, Input, InputWrapper} from "./styled";
+import {SearchHeaderWrapper, Input, CancelText} from "./styled";
 import SearchIcon from "../SearchIcon";
+import {Animated, StyleSheet, Keyboard} from 'react-native';
+import Layout from "../../constants/Layout";
+import {TouchableWithoutFeedback} from "react-native-web";
+
+let WIDTH = Layout.window.width - 35;
 
 export const SearchHeader = () => {
-  return(
+  let width = new Animated.Value(WIDTH);
+  let clearRef = React.createRef();
+
+  const handleStartAnimation = () => {
+    Animated.timing(width, {
+      toValue: WIDTH - 60,
+      duration: 300,
+    }).start()
+  };
+
+  const handleStopAnimation = () => {
+    Animated.timing(width, {
+      toValue: WIDTH,
+      duration: 300,
+    }).start()
+  };
+
+  const handleClearance = () => {
+    clearRef.current.clear();
+    Keyboard.dismiss();
+    return handleStopAnimation()
+  };
+
+  return (
     <SearchHeaderWrapper>
-      <InputWrapper>
+      <Animated.View style={[styles.InputWrapper, {width: width}]}>
         <SearchIcon
           focused
           height={18}
@@ -14,11 +42,34 @@ export const SearchHeader = () => {
           stroke='#7c7c7c'
         />
         <Input
+          ref={clearRef}
+          onFocus={handleStartAnimation}
+          onBlur={handleStopAnimation}
           clearButtonMode='while-editing'
           placeholderTextColor='#7c7c7c'
-          placeholder='Mavel’s The Defender'
+          placeholder='Try Mavel’s The Defender'
         />
-      </InputWrapper>
+
+      </Animated.View>
+      <TouchableWithoutFeedback onPress={handleClearance} style={{}}>
+        <CancelText style={{color: '#fff'}}>Cancel</CancelText>
+      </TouchableWithoutFeedback>
     </SearchHeaderWrapper>
   )
 };
+
+const styles = StyleSheet.create({
+  InputWrapper: {
+    marginRight: 14,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    height: 46,
+    borderRadius: 5,
+    backgroundColor: '#2b2b2b',
+    borderWidth: 1,
+    borderColor: '#707070',
+    paddingVertical: 13,
+    paddingRight: 5,
+    paddingLeft: 13,
+  }
+})
--- components/SearchHeader/styled.js
+++ components/SearchHeader/styled.js
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 
 export const SearchHeaderWrapper  = styled.View`
   padding:20px 20px;
+  flex-direction:row;
+  align-items:center;
 `;
 
 
@@ -14,14 +16,11 @@ export const Input = styled.TextInput`
   color: #8b8b8b;
 `;
 
-export const InputWrapper = styled.View`
-  flex-direction:row;
-  justify-content:space-between;
-  width: 100%;
-  height: 46px;
-  border-radius: 5px;
-  background: #2b2b2b;
-  border: 1px solid #707070;
-  padding:13px 5px 13px 13px;
+export const CancelText = styled.Text`
+  font-family: cinema;
+  font-size: 15px;
+  text-align: left;
+  color: #8b8b8b;
 `;
+
 
